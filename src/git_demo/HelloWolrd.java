e9a608a6fe6ac2823c992ae1a422bcbd2270439f
zhaoxianglinZXL <com>
2018-03-14 18:23:53 +0800

    添加了一个类

--- src/git_demo/HelloWolrd.java
+++ src/git_demo/HelloWolrd.java
@@ -5,7 +5,7 @@ public class HelloWolrd {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("dfwecvxccvcsdfsffdsfvxcvxvxcvxcvvcx");
-		System.out.println("dfwecvxccvcsdfsffdsfvxcvxvxcvxcvvcx");
+		System.out.println("dfwecsdfdfvxccvcsdfsffdsfvxcvxvxcvxcvvcx");
 		System.out.println("只有这样了嘛");
 	}
 
